e9832720fe28e471d81646cef05d14efd94315ec
Aleksandr Babaskin <1990aleksandrbabaskin@gmail.com>
2026-01-13 14:53:54 +0200

    add HomePage and QuestionCard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainLayout } from "./assets/components/MainLayout";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainLayout />} />
-        <Route path="/home" element={<div>Home</div>} />
-        <Route path="/forbidden" element={<div>Forbidden</div>} />
-        <Route path="/addquestion" element={<div>Add Question</div>} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="forbidden" element={<div>Forbidden</div>} />
+          <Route path="addquestion" element={<div>Add Question</div>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
