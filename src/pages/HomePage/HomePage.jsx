63c2203a51332a54f44cb158906065724d3a405a
Aleksandr Babaskin <1990aleksandrbabaskin@gmail.com>
2026-01-14 15:19:27 +0200

    useEffect, move db, api_url

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,30 @@
 import { QuestionCard } from "../../assets/components/QuestionCard";
 import cls from "./HomePage.module.css";
-
-// Временные данные для тестирования
-const cards = [
-  { id: 1, level: 1, completed: false, question: "What is React?", answer: "A JavaScript library for building user interfaces" },
-];
+import { API_URL } from "../../constants";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const HomePage = () => {
+  const [questions, setQuestions] = useState([]);
+
+  const getQuestions = async () => {
+    try {
+      const response = await fetch(`${API_URL}/react`);
+      const questions = await response.json();
+      setQuestions(questions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
   return (
     <>
-      {cards.map((card, index) => {
-        return <QuestionCard card={card} key={index} />;
+      {questions.map((question, index) => {
+        return <QuestionCard card={question} key={index} />;
       })}
     </>
   );
